0e423a2ba9b8a7d2239ffbb921cc8b72820ae55e
adit1609 <140595403+adit1609@users.noreply.github.com>
2024-01-31 15:46:49 +0530

    Add files via upload
    For Accessing the database and the employee detail this database can be used.

---
+++ Database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Aaditya;
+USE Aaditya;
+CREATE TABLE Employee(
+Employee_ID varchar(50),
+First_Name varchar(50),
+Last_Name varchar(50),
+Email_ID varchar(70)
+);
+SELECT * FROM Employee;
+DROP TABLE Employee;
