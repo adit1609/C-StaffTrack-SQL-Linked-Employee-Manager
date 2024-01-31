e553fb065ee94a24cbcdc7498c4f37bef5f975f9
adit1609 <140595403+adit1609@users.noreply.github.com>
2024-01-31 15:23:58 +0530

    Create Form1.Designer.cs
    This is the code for the exe file (Windows Form)

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+namespace NMTronis_Aaditya
+{
+    partial class FF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Employee_ID = new System.Windows.Forms.Label();
+            this.tbemployeeID = new System.Windows.Forms.TextBox();
+            this.First_Name = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastname = new System.Windows.Forms.TextBox();
+            this.tbEmailID = new System.Windows.Forms.TextBox();
+            this.btSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Employee_ID
+            // 
+            this.Employee_ID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Employee_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Employee_ID.Location = new System.Drawing.Point(58, 74);
+            this.Employee_ID.Name = "Employee_ID";
+            this.Employee_ID.Size = new System.Drawing.Size(90, 24);
+            this.Employee_ID.TabIndex = 0;
+            this.Employee_ID.Text = "Employee_ID";
+            this.Employee_ID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbemployeeID
+            // 
+            this.tbemployeeID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbemployeeID.Location = new System.Drawing.Point(168, 73);
+            this.tbemployeeID.Name = "tbemployeeID";
+            this.tbemployeeID.Size = new System.Drawing.Size(165, 20);
+            this.tbemployeeID.TabIndex = 1;
+            // 
+            // First_Name
+            // 
+            this.First_Name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.First_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.First_Name.Location = new System.Drawing.Point(58, 136);
+            this.First_Name.Name = "First_Name";
+            this.First_Name.Size = new System.Drawing.Size(90, 23);
+            this.First_Name.TabIndex = 2;
+            this.First_Name.Text = "First_Name";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last_Name";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(58, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email_ID";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbFirstName.Location = new System.Drawing.Point(168, 139);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(165, 20);
+            this.tbFirstName.TabIndex = 5;
+            // 
+            // tbLastname
+            // 
+            this.tbLastname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbLastname.Location = new System.Drawing.Point(168, 216);
+            this.tbLastname.Name = "tbLastname";
+            this.tbLastname.Size = new System.Drawing.Size(165, 20);
+            this.tbLastname.TabIndex = 6;
+            // 
+            // tbEmailID
+            // 
+            this.tbEmailID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbEmailID.Location = new System.Drawing.Point(168, 271);
+            this.tbEmailID.Name = "tbEmailID";
+            this.tbEmailID.Size = new System.Drawing.Size(165, 20);
+            this.tbEmailID.TabIndex = 7;
+            this.tbEmailID.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // btSave
+            // 
+            this.btSave.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSave.Location = new System.Drawing.Point(194, 312);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(93, 32);
+            this.btSave.TabIndex = 8;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = false;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "EMPLOYEE MANAGMENT";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::NMTronis_Aaditya.Properties.Resources.logorev;
+            this.pictureBox1.Location = new System.Drawing.Point(390, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 243);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(491, 367);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.tbEmailID);
+            this.Controls.Add(this.tbLastname);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.First_Name);
+            this.Controls.Add(this.tbemployeeID);
+            this.Controls.Add(this.Employee_ID);
+            this.Name = "FF";
+            this.Text = "Employee_Details";
+            this.Load += new System.EventHandler(this.FF_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Employee_ID;
+        private System.Windows.Forms.TextBox tbemployeeID;
+        private System.Windows.Forms.Label First_Name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastname;
+        private System.Windows.Forms.TextBox tbEmailID;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
